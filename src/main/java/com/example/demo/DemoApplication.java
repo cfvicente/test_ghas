969dab89adbecede4f8daaab68e40474fef6b8ca
cvicente <claudiofpvicente@tutanota.com>
2026-04-30 11:43:19 +0100

    dsd

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,8 +1,5 @@
 package com.example.demo;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,8 +8,6 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		Logger logger = Logger.getLogger("Dsdsd");
-		logger.log(Level.INFO, "Xpto");
 	}
 
 }
