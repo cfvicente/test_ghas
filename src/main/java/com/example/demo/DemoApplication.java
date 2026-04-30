a548903644fcdb2e7441c1365e87755560a96ef9
cvicente <claudiofpvicente@tutanota.com>
2026-04-30 09:33:54 +0100

    log

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +11,8 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		Logger logger = Logger.getLogger("Dsdsd");
+		logger.log(Level.INFO, "Xpto");
 	}
 
 }
